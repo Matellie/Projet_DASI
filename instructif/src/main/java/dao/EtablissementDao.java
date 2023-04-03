b4f1c62689b79556d51d52e2ac52d79ee6c02e6a
mhabran <mhabran@2D-3IF-DASI-14.vdi.insa-lyon.fr>
2023-04-03 17:57:31 +0200

    beauty fix

--- instructif/src/main/java/dao/EtablissementDao.java
+++ instructif/src/main/java/dao/EtablissementDao.java
@@ -13,29 +13,28 @@ import metier.modele.Etablissement;
  * @author mhabran
  */
 public class EtablissementDao {
-    
+
     public EtablissementDao() {
     }
-    
+
     public void create(Etablissement etablissement) {
         JpaUtil.obtenirContextePersistance().persist(etablissement);
     }
-    
-    public Etablissement getEtablissement(String uai){
+
+    public Etablissement getEtablissement(String uai) {
         String s = "select e from Etablissement e where e.uai = :leUai";
-        
+
         TypedQuery query = JpaUtil.obtenirContextePersistance().createQuery(s, Etablissement.class);
         query.setParameter("leUai", uai);
-        
+
         Etablissement etab = null;
-        
-        try{
-            etab = (Etablissement)query.getSingleResult();
+
+        try {
+            etab = (Etablissement) query.getSingleResult();
+        } catch (Exception ex) {
+
         }
-        catch (Exception ex) {
-            
-        }        
         return etab;
     }
-    
+
 }
